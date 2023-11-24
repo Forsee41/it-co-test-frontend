3713df1e274fd8d27d2948ed1ffd7258e13ff95a
Forsee41 <forsee41@gmail.com>
2023-11-24 07:01:16 +0300

    add link attribute to default project

--- src/store/index.js
+++ src/store/index.js
@@ -20,6 +20,7 @@ export default createStore({
           name: '',
           image: '',
           description: '',
+          link: '',
         }
         if (id === 'new') {
           return blankProject
